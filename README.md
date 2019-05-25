2837c1f6a39ceecd0d77f535de598cdf9e2e4e95
whoami90506 <whoami90506@github.com>
2019-05-25 21:43:29 +0800

    show text

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # web_learning
 ## Node.js
-
+### reference
 [Node入門](https://www.nodebeginner.org/index-zh-tw.html)
+###problem
+* context of web (HTML/CSS/MVC) ?
+* event ofr listener ?
+* usage of npm
+    - package json
+    - run ?
--- nodejs/requestHandlers.js
+++ nodejs/requestHandlers.js
@@ -1,4 +1,6 @@
-function start(response) {
+var querystring = require("querystring");
+
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
@@ -19,10 +21,10 @@ function start(response) {
     response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " + querystring.parse(postData).text);
   response.end();
 }
 
